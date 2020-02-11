7e3488f1f8487946f1e7880317dd6c96208783e8
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2020-02-11 17:26:19 +0100

    aggiunto un secondo test

--- src/test/java/carrellottf/backend/MainTest.java
+++ src/test/java/carrellottf/backend/MainTest.java
@@ -11,4 +11,11 @@ class MainTest {
             throw new Exception();
     }
 
+    @Test
+    void userTest2() throws Exception {
+        User u = UserRepository.getInstance().get("user2");
+        if (!u.getLogin().equals("user2"))
+            throw new Exception();
+    }
+
 }
